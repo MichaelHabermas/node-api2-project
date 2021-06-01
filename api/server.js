9a8897ac6ad47b036f1d19045ec93cefa80dac03
MichaelHabermas <michaelghabermas@gmail.com>
2021-06-01 11:48:26 -0400

    wip

--- api/server.js
+++ api/server.js
@@ -1,2 +1,8 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express');
+
+const Posts = require('.posts/posts-model');
+const server = express();
+
+server.use(express.json());
